1e45af7713d776796bf82b27c90a132324d576ff
naipaka <naipakapaka@gmail.com>
2020-09-23 12:52:57 +0900

    Initial Commit

---
+++ FaceDetectionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetectionApp
+//
+//  Created by 小林遼太 on 2020/09/23.
+//  Copyright © 2020 小林遼太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
